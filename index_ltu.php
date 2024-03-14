3d5301d526d1162c34ff50ba6df587e7612fd25b
daggelito02 <daggelito02@gmail.com>
2024-03-14 19:18:41 +0100

    uppgift 3a

---
+++ index_ltu.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <title>Dag Fredriksson Luleå Universitet</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
+    <style>
+      body {
+        margin-left: 2rem;
+      }
+      ul {
+        list-style-type: none; 
+        padding: 0; 
+        margin: 0; 
+      }
+      li {
+        margin-bottom: .4rem;
+      }
+      .material-symbols-outlined {
+        font-size: 1rem;
+        position: relative ;
+        top: 3px;
+        padding-left: 4px;
+        background: white;
+        right: 5px;
+      }
+      .material-symbols-outlined.green {
+        color:green;
+      }
+      .material-symbols-outlined + a {
+          color: #333;
+          text-decoration: none;
+      }
+      .material-symbols-outlined + a:hover {
+          color: #333;
+          text-decoration: underline;
+      }
+      </style>
+  </head>
+  <body>
+    <h1>Uppgifter Webbutveckling II</h1>
+    <p>Länklista för Labb 1a - PHP-sidor</p>
+    <ul>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida1.php">
+        Strängar &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida2.php">
+        Arrayer &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida3.php">
+        Loopar och villkorssatser &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida4.php">
+        Funktioner &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida5.php">
+        Servervariabler &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-1a/sida6.html">
+          Meddelandehantering &raquo;
+        </a>
+      </li>
+    </ul>
+    <p>Länklista för Labb 2a - PHP-sidor</p>
+    <ul>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-2a/login.php">
+        login &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-2a/index.php">
+        index &raquo;
+        </a>
+      </li>
+    </ul>
+    <p>Länklista för Labb 3a - Enkla Javascript-sidor</p>
+    <ul>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-3a/task_one.html">
+          Koduppgift ett &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-3a/task_two.html">
+          Koduppgift två &raquo;
+        </a>
+      </li>
+      <li>
+        <span class="material-symbols-outlined green">check_box</span>
+        <a href="~dagfre-3/labb-3a/task_three.html">
+          Koduppgift tre &raquo;
+        </a>
+      </li>
+    </ul>
+  </body>
+</html>
