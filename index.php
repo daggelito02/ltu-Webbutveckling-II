9b0f7837814ad3956acfaafb0c47f70906db8dda
daggelito02 <daggelito02@gmail.com>
2024-09-29 14:29:39 +0200

    Ny grundstruktur

--- index.php
+++ index.php
@@ -40,42 +40,45 @@
       </style>
   </head>
   <body>
+    <p>
+      <a href="~dagfre-3/index.html">&laquo; Överblick kurser</a>
+    </p>
     <h1>Uppgifter Webbutveckling II</h1>
     <p>Länklista för Labb 1a - PHP-sidor</p>
     <ul>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida1.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida1.php">
         Strängar &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida2.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida2.php">
         Arrayer &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida3.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida3.php">
         Loopar och villkorssatser &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida4.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida4.php">
         Funktioner &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida5.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida5.php">
         Servervariabler &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-1a/sida6.html">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-1a/sida6.html">
           Meddelandehantering &raquo;
         </a>
       </li>
@@ -84,13 +87,13 @@
     <ul>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-2a/login.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-2a/login.php">
         login &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-2a/index.php">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-2a/index.php">
         index &raquo;
         </a>
       </li>
@@ -99,20 +102,29 @@
     <ul>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-3a/task_one.html">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-3a/task_one.html">
           Koduppgift ett &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-3a/task_two.html">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-3a/task_two.html">
           Koduppgift två &raquo;
         </a>
       </li>
       <li>
         <span class="material-symbols-outlined green">check_box</span>
-        <a href="ltu-Webbutveckling-II/labb-3a/task_three.html">
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-3a/task_three.html">
           Koduppgift tre &raquo;
+        </a>
+      </li>
+    </ul>
+    <p>Länklista för Labb 4a - Utveckla en blogg</p>
+    <ul>
+      <li>
+        <span class="material-symbols-outlined">check_box</span>
+        <a href="~dagfre-3/ltu-Webbutveckling-II/labb-4a/index.php">
+          Labb 4a - Utveckla en blogg &raquo;
         </a>
       </li>
     </ul>
