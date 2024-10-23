b2923e8b98ed734817412d85d4af4f9d2c1e4291
daggelito02 <daggelito02@gmail.com>
2024-10-23 11:04:29 +0200

    test

--- labb-4a/admin/db_credentials.php
+++ labb-4a/admin/db_credentials.php
@@ -4,8 +4,11 @@
     // define("DB_PASS", "");
     // define("DB_NAME", "CMS");
 
+    //define("DB_SERVER", "utbweb.its.ltu.se:3308");
     define("DB_SERVER", "utbweb.its.ltu.se:3308");
     define("DB_USER", "dagfre3");
     define("DB_PASS", "jcmc8ncJ");
     define("DB_NAME", "D0019E_V24_dagfre3");
+
+    //https://students.www.ltu.se/:3308
 ?>
